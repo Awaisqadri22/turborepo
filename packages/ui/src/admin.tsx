6447299eb80d40d7a328701c9a4134e52f0d4bc0
Awaisqadri22 <42121572+Awaisqadri22@users.noreply.github.com>
2024-08-08 12:35:30 +0200

    start

---
+++ packages/ui/src/admin.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export const Admin = () => {
+  return (
+    <>
+      <h1>hi from admin component</h1>;<h1>hi from admin component</h1>;
+      <h1>hi from admin component</h1>;<h1>hi from admin component</h1>;
+      <h1>hi from admin component</h1>;<h1>hi from admin component</h1>;
+    </>
+  );
+};
